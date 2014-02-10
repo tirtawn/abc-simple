fb09615a09467abf52e9a6d33b73aa90536464b7
John Reed <johnnycarlos@gmail.com>
2014-02-10 14:06:55 -0800

    Add case statement to switch between two letters

--- src/com/johnnycarlos/abc_simple/MainActivity.java
+++ src/com/johnnycarlos/abc_simple/MainActivity.java
@@ -4,49 +4,60 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.graphics.drawable.Drawable;
 import android.util.Log;
-import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.ImageView;
 
 public class MainActivity extends Activity {
-
-
+	
+	private alphabet currentLetter = alphabet.A;
+	private enum alphabet { A, B, C }
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		runGame();
 		
-        ImageView imageView = (ImageView)findViewById(R.id.main_image_id);
-	
-        Drawable nextImage;      
-        nextImage = getResources().getDrawable(R.drawable.b);
-        imageView.setImageDrawable(nextImage);
-        
+	}
+
+    private void runGame(){
+
+    	// Load image for letter A
+        final ImageView imageView = (ImageView)findViewById(R.id.main_image_id);
+
+        // Set a listener and change to letter B when clicked
         imageView.setOnClickListener(new OnClickListener() {
-            public void onClick(View v) {
-            	Log.d("logging", "Inside Listener!");
-            }
-        });
         
-  
+        	public void onClick(View v) {
+        		
+        		int currentLetterImage = R.drawable.a;
+        		
+        		switch (currentLetter){
+  				    case A:  
+  				        currentLetter = alphabet.B;
+  				        currentLetterImage = R.drawable.b;
+				    	break;
+				    case B:  
+				        currentLetter = alphabet.A;
+				        currentLetterImage = R.drawable.a;
+			    		break;
+				    case C:
+					    break;
+				    default:
+					    break;        		        		
+        		}
+        		
+            	Log.d("logging", "Inside runGame Listener!");
+                Drawable nextImage;      
+                nextImage = getResources().getDrawable( currentLetterImage );
+                imageView.setImageDrawable(nextImage);
+            
+        	}
         
-		// follow up with peekaboo
-		
-		// need add another image otherwise i don't know if it is just
-		// coming from the layout/activity_main.xml
-		
-	}
+        }); // end setOnClickListener
+        
+    } // end runGame
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-
-	public void imageClicked(View view) {  
-		 Log.d("logging", "Letter A was clicked!");
-    } 
-	
-}
+} // end class
