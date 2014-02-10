9756c3d11c8a2278374fe0d009e9f266d2bde94c
John Reed <johnnycarlos@gmail.com>
2014-02-10 12:51:28 -0800

    Add loading a second image and click listener

--- src/com/johnnycarlos/abc_simple/MainActivity.java
+++ src/com/johnnycarlos/abc_simple/MainActivity.java
@@ -2,9 +2,12 @@ package com.johnnycarlos.abc_simple;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.graphics.drawable.Drawable;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageView;
 
 public class MainActivity extends Activity {
 
@@ -13,6 +16,26 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+        ImageView imageView = (ImageView)findViewById(R.id.main_image_id);
+	
+        Drawable nextImage;      
+        nextImage = getResources().getDrawable(R.drawable.b);
+        imageView.setImageDrawable(nextImage);
+        
+        imageView.setOnClickListener(new OnClickListener() {
+            public void onClick(View v) {
+            	Log.d("logging", "Inside Listener!");
+            }
+        });
+        
+  
+        
+		// follow up with peekaboo
+		
+		// need add another image otherwise i don't know if it is just
+		// coming from the layout/activity_main.xml
+		
 	}
 
 	@Override
@@ -22,7 +45,7 @@ public class MainActivity extends Activity {
 		return true;
 	}
 
-	public void aImageClicked(View view) {  
+	public void imageClicked(View view) {  
 		 Log.d("logging", "Letter A was clicked!");
     } 
 	
